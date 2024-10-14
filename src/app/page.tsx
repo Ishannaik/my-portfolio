090096a66ef6079d12512fa0e4cb0093c0cb5d21
Ishan <ishannaik7@gmail.com>
2024-10-14 21:30:48 +0530

    Updated stuff bento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
-import TimelineComponent from "@/components/TimelineComponent";
+// import TimelineComponent from "@/components/TimelineComponent";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div>
+      <FloatingNav
+        navItems={[
+          { name: "About", link: "#about", icon: <FaHome /> },
+          { name: "Experience", link: "#experience", icon: <FaHome /> },
+          { name: "Projects", link: "#projects", icon: <FaHome /> },
+          { name: "Contact", link: "#contact", icon: <FaHome /> },
+        ]}
+      />
       <Hero />
-      <TimelineComponent />
+      <Grid />
+      {/* <TimelineComponent />*/}
     </div>
   );
 }
